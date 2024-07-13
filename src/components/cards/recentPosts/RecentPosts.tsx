41e69d54dbbd52a040c81e6a9ad000e6ae4d9269
muhammed ali <muhammedalip703@gmail.com>
2024-07-13 19:11:13 +0530

    Categories and RecentPosts card added in home page

---
+++ src/components/cards/recentPosts/RecentPosts.tsx
@@ -0,0 +1,31 @@
+const RecentPosts = () => {
+  return (
+    <div>
+      <div
+        className="block max-w-sm p-6 mx-auto mt-4  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+          Recent Posts
+        </h5>
+        <div className="my-5">
+        <ul>
+          <li>
+            <p className="font-normal text-gray-700">Web Development </p>
+          </li>
+          <li>
+            <p className="font-normal text-gray-700">Design</p>
+          </li>
+          <li>
+            <p className="font-normal text-gray-700">Programming</p>
+          </li>
+          <li>
+            <p className="font-normal text-gray-700">Technology</p>
+          </li>
+        </ul>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default RecentPosts
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,22 @@
+import CategoriesComponent from "@/components/cards/categories/Categories";
+import HomeCardComponent from "@/components/cards/homeCard/HomeCard";
+import RecentPosts from "@/components/cards/recentPosts/RecentPosts";
 import NavbarComponent from "@/components/navbar/NavbarComponent";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <NavbarComponent title="blogsphere" />
-    </div>
+      <div className="container ml-10 mt-5 flex flex-row space-x-4">
+        <div className="w-[75%]">
+          <HomeCardComponent />
+        </div>
+        <div className="w-[25%]">
+          <CategoriesComponent />
+          <RecentPosts />
+        </div>
+      </div>
+    </>
   );
 };
 
