6590c2c793460473b002f8844bcff89cc82b0ee8
muhammed ali <muhammedalip703@gmail.com>
2024-07-13 15:34:07 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import * as React from 'react';
+import './App.css'
+import HomePage from '@/pages/home/HomePage';
+
+
+const App:React.FC = () =>{
+
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,11 @@
+import NavbarComponent from "@/components/navbar/NavbarComponent"
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavbarComponent title='blogsphere'/>
+    </div>
+  )
+}
+
+export default HomePage
