0e1e387f52e9942f94f0d7310b527654be784a7e
muhammed ali <muhammedalip703@gmail.com>
2024-07-23 15:27:58 +0530

    login page created

---
+++ src/components/form/login/Login.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+// import Cookies from "js-cookie";
+import { Link, useNavigate } from "react-router-dom";
+import { AUTH_SERVICES } from "@/utils/services/api/authServices";
+import { useAppDispatch } from '@/utils/helpers/hook/ReduxHook'
+import { setCredentials } from "@/utils/helpers/auth/authSlice";
+// import { setCredentials } from '@/utils/helpers/auth/authSlice'; // Import the setCredentials action
+
+const LoginComponent = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch(); // Use the dispatch hook
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    AUTH_SERVICES.login(username, password).then((res) => {
+      if (res.status === 200) {
+        dispatch(setCredentials(res.data))
+        return navigate("/");
+      }
+    });
+  };
+
+
+  return (
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Sign in
+            </h1>
+            <form
+              className="space-y-4 md:space-y-6"
+              action="#"
+              onSubmit={handleSubmit}
+            >
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Your username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="username"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label className="text-gray-500">Remember me</label>
+                  </div>
+                </div>
+                <a
+                  href="#"
+                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-dark bg-gray-200 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center border"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500">
+                Don’t have an account yet?{" "}
+                <Link
+                  to="/register"
+                  className="font-medium text-primary-600 hover:underline"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginComponent;
