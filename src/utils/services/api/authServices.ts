81033942a8d2f6e04fd323927a8099c8ab581b8d
muhammed ali <muhammedalip703@gmail.com>
2024-07-26 20:38:43 +0530

    feat():registration api created

--- src/utils/services/api/authServices.ts
+++ src/utils/services/api/authServices.ts
@@ -1,7 +1,11 @@
 import axiosInstance from './api';
+import { RegistrationUserData } from '@/utils/helpers/constants/interfaces';
 
 export const AUTH_SERVICES = {
   login: async (username:string, password:string) => {
     return await axiosInstance.post('user/token', { username, password })
+  },
+  registration: async (userData:RegistrationUserData) => {
+    return await axiosInstance.post('user/sign-up', userData)
   },
 };
