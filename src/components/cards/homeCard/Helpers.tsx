8ca87812f716f4813656e2af5b024954332ce4c8
muhammed ali <muhammedalip703@gmail.com>
2024-07-27 12:50:23 +0530

    optimization():homeCard helper functions optimized

--- src/components/cards/homeCard/Helpers.tsx
+++ src/components/cards/homeCard/Helpers.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { AxiosResponse } from "axios";
 import { BLOG_SERVICES } from "@/utils/services/api/blogServices";
 import { HomeCardComponentInterface } from "@/utils/helpers/constants/interfaces";
@@ -8,7 +9,9 @@ export const HOMEPAGE_HOOK_HELPERS = {
   useManageHomePage: () => {
     const [blogs, setBlogs] = React.useState<HomeCardComponentInterface>();
     const navigate = useNavigate();
-
+    const handleClickCard=(id:string)=>{
+      navigate(`blog/${id}`)
+    }
     React.useEffect(() => {
       void BLOG_SERVICES.getBlogs()
         .then((res: AxiosResponse) => {
@@ -24,6 +27,40 @@ export const HOMEPAGE_HOOK_HELPERS = {
 
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    return { blogs, setBlogs };
+    React.useEffect(() => {
+      const handleScroll = (e: Event) => {
+        const target = e.target as Document | null; // Cast e.target to Document or null
+  
+        if (!target) {
+          return; // Handle case where e.target is null
+        }
+        const scrollHeight = target.documentElement.scrollHeight;
+        const currentHeight =
+          target.documentElement.scrollTop + window.innerHeight;
+        if (currentHeight + 1 >= scrollHeight) {
+          if (blogs?.next) {
+            axios
+              .get(blogs.next)
+              .then(function (response) {
+                const data: HomeCardComponentInterface = response.data.data;
+                setBlogs((prev) => ({
+                  ...prev,
+                  results: [...prev!.results, ...data.results],
+                  count: data.count,
+                  next: data.next,
+                  previous: data.previous,
+                }));
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }
+        }
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }, [blogs, setBlogs]);
+  
+    return { blogs, setBlogs, handleClickCard };
   },
 };
--- src/components/cards/homeCard/HomeCard.tsx
+++ src/components/cards/homeCard/HomeCard.tsx
@@ -1,51 +1,19 @@
-import React from "react";
-import axios from "axios";
+
 import { HOMEPAGE_HOOK_HELPERS } from "./Helpers";
-import { HomeCardComponentInterface } from "@/utils/helpers/constants/interfaces";
 
 const HomeCardComponent = () => {
-  const { blogs, setBlogs } = HOMEPAGE_HOOK_HELPERS.useManageHomePage();
-  React.useEffect(() => {
-    const handleScroll = (e: Event) => {
-      const target = e.target as Document | null; // Cast e.target to Document or null
-
-      if (!target) {
-        return; // Handle case where e.target is null
-      }
-      const scrollHeight = target.documentElement.scrollHeight;
-      const currentHeight =
-        target.documentElement.scrollTop + window.innerHeight;
-      if (currentHeight + 1 >= scrollHeight) {
-        if (blogs?.next) {
-          axios
-            .get(blogs.next)
-            .then(function (response) {
-              const data: HomeCardComponentInterface = response.data.data;
-              setBlogs((prev) => ({
-                ...prev,
-                results: [...prev!.results, ...data.results],
-                count: data.count,
-                next: data.next,
-                previous: data.previous,
-              }));
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-        }
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [blogs, setBlogs]);
-
+  const { blogs, handleClickCard } = HOMEPAGE_HOOK_HELPERS.useManageHomePage();
+  
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {blogs?.results.map((item, index) => (
+        {blogs?.results.map((item) => (
           <div
-            key={index}
+            key={item.id}
             className="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
+            onClick={() => {
+              handleClickCard(item.id.toString()); // Convert id to string here
+            }}
           >
             <a href="#">
               <img
