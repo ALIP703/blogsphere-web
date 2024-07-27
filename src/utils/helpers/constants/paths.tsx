5f1a684cdeb0ba025662bdac3b1eeef302e2cd68
muhammed ali <muhammedalip703@gmail.com>
2024-07-27 12:49:36 +0530

    bugfix: blog path modified()

--- src/utils/helpers/constants/paths.tsx
+++ src/utils/helpers/constants/paths.tsx
@@ -2,7 +2,7 @@ import { PageRoutes } from "./interfaces"
 
 const PAGES:PageRoutes={
     DEFAULT:'/',
-    BLOG_PAGE:'/blog',
+    BLOG_PAGE:'/blog/:id',
     LOGIN:'/login',
     SIGN_UP:"/register",
     CREATE_BLOG_PAGE:'/create-blog',
