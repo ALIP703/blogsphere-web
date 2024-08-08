acebee51911976bb8c6ae0857c1ec6bfa103e281
muhammed ali <muhammedalip703@gmail.com>
2024-08-08 11:08:02 +0530

    enhancement():enhance responsiveness

--- src/components/cards/homeCard/HomeCard.tsx
+++ src/components/cards/homeCard/HomeCard.tsx
@@ -5,7 +5,7 @@ const HomeCardComponent = () => {
   const { blogs, handleClickCard } = HOMEPAGE_HOOK_HELPERS.useManageHomePage();
   
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 flex items-center justify-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {blogs?.results.map((item) => (
           <div
--- src/components/cards/recentPosts/RecentPosts.tsx
+++ src/components/cards/recentPosts/RecentPosts.tsx
@@ -2,7 +2,7 @@ const RecentPosts = () => {
   return (
     <div>
       <div
-        className="block max-w-sm p-6 mx-auto mt-4  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+        className="block max-w-sm  p-3 lg:p-6  mx-auto mt-4  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
       >
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
           Recent Posts
