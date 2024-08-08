0d29f928a1b2d40f4460bffc88c6a62fffc52edc
muhammed ali <muhammedalip703@gmail.com>
2024-08-08 11:00:02 +0530

    enhancement(navbar):tooltip added and unwanted code removed

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -5,6 +5,7 @@ import { RootState } from "@/utils/helpers/auth/store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"; // Import the close icon
 import DropDownProfileComponent from "../dropDownProfile/DropDownProfile";
+import ToolTip from "../toolTip/ToolTip";
 
 interface HeaderProps {
   title: string;
@@ -30,58 +31,41 @@ export default function NavbarComponent(props: HeaderProps) {
             </span>
           </Link>
           <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <Link to={"/create-blog"} className="mr-4 mt-1.5">
-              <FontAwesomeIcon icon={faPenToSquare} className=" w-5 h-5" />
-            </Link>
+            <ToolTip message="Write">
+              <Link to={"/create-blog"} className="mr-4 mt-1.5 hidden lg:block">
+                <FontAwesomeIcon icon={faPenToSquare} className=" w-5 h-5" />
+              </Link>
+            </ToolTip>
             {user.username == "" || null ? (
               <Link to={"/login"}>
                 <span className="font-semibold">Login</span>
               </Link>
             ) : (
-              <div>
-                <button
-                  type="button"
-                  className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  data-dropdown-toggle="user-dropdown"
-                  data-dropdown-placement="bottom"
-                  onClick={() => setOpenProfileMenu(!openProfileMenu)}
-                >
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="w-8 h-8 rounded-full"
-                    src={`${import.meta.env.VITE_BASE_URL}${user?.image}`}
-                    alt="user photo"
-                  />
-                </button>
-                {openProfileMenu && <DropDownProfileComponent openProfileMenu={openProfileMenu} setOpenProfileMenu={setOpenProfileMenu} />}
+              <div className="relative flex items-center">
+                <ToolTip message="Profile">
+                  <button
+                    type="button"
+                    className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    onClick={() => setOpenProfileMenu(!openProfileMenu)}
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src={`${import.meta.env.VITE_BASE_URL}${user?.image}`}
+                      alt="user photo"
+                    />
+                  </button>
+                </ToolTip>
+                {openProfileMenu && (
+                    <DropDownProfileComponent
+                      openProfileMenu={openProfileMenu}
+                      setOpenProfileMenu={setOpenProfileMenu}
+                    />
+                )}
               </div>
             )}
-            <button
-              data-collapse-toggle="navbar-user"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-              aria-controls="navbar-user"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
           </div>
         </div>
       </nav>
